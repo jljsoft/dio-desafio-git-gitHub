6ef753c3d907939090b63c844c098b23aa711e5b
Joao Lucas <lucasjesussoft@gmail.com>
2022-05-12 16:36:11 -0300

    Trabalhando com entradas de dados com C#

---
+++ Estruturas de Controle - C#/aula-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace aula_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1;
+            int valor2;
+
+            WriteLine("Digite o primeiro valor:");
+            valor1 = int.Parse(ReadLine());
+            WriteLine("Primeiro valor é: " + valor1);
+
+            WriteLine("Digite o segundo valor:");
+            valor2 = int.Parse(ReadLine());
+            WriteLine("Segundo valor é: " + valor2);
+
+            int total = valor1 + valor2;
+
+            WriteLine($"A soma dos 2 valores é: {total}");
+        }
+    }
+}
