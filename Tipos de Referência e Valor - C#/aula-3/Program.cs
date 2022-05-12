3e86c5ec31aabcb0a0bfdeec39af08da734e053c
Joao Lucas <lucasjesussoft@gmail.com>
2022-05-12 15:43:41 -0300

    Trabalhando com ref struct

---
+++ Tipos de Referência e Valor - C#/aula-3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace aula_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "João Lucas";
+            p1.Idade = 23;
+
+            p1.EnderecoPessoa = new Endereco()
+            {
+            Logradouro = "Rua teste",
+            Numero = 300,
+            CEP = "000000",
+            Cidade = "Teresina",
+            };
+
+            WriteLine("Fim");
+        }
+    }
+}
